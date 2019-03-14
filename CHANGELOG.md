538c6962ab22e6b7b59e6768b763dbe621804247
lykmapipo <lallyelias87@gmail.com>
2019-03-14 12:37:55 +0300

    chore: generate latest changelog & docs

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+#### 0.1.0 (2019-03-14)
+
+##### New Features
+
+*  implement predefined model and rest api ([cf72a504](https://github.com/lykmapipo/predefine/commit/cf72a50495bf8f00676ed2603c439d34c478ab48))
+
+##### Refactors
+
+*  remove predefine collection field ([accc0a47](https://github.com/lykmapipo/predefine/commit/accc0a477cd3debf1b5078fe5bf7a0dde9717662))
+*  allow namespace to be out of model names ([936e401c](https://github.com/lykmapipo/predefine/commit/936e401c78d816120c3b4ef7e7c9bb0ae6dd083a))
+
---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "predefine",
+  "version": "1.0.0",
+  "description": "A representation of stored and retrieved information that does not qualify to belongs to their own domain model.",
+  "title": "Predefine",
+  "url": "https://predefine.herokuapp.com/v1",
+  "sampleUrl": "https://predefine.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-14T09:37:29.345Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
