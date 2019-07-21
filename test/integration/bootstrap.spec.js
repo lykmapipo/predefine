4d88f9927b9a26d0bc0d3e90b5fb2c022c9c7c19
lykmapipo <lallyelias87@gmail.com>
2019-07-21 14:02:48 +0300

    test: add relation create specs

--- test/integration/bootstrap.spec.js
+++ test/integration/bootstrap.spec.js
@@ -4,6 +4,8 @@ process.env.NODE_ENV = 'test';
 process.env.DEFAULT_LOCALE = 'en';
 process.env.LOCALES = 'en,sw';
 process.env.PREDEFINE_NAMESPACES = 'Setting,Currency,Unit';
+process.env.PREDEFINE_RELATIONS =
+  '{"priority":{"ref":"Predefine"},"status":{}}';
 
 /* setup database */
 before(done => connect(done));
--- test/integration/predefine.post.spec.js
+++ test/integration/predefine.post.spec.js
@@ -1,4 +1,4 @@
-import { expect, clear } from '@lykmapipo/mongoose-test-helpers';
+import { expect, clear, create } from '@lykmapipo/mongoose-test-helpers';
 import { Predefine } from '../../src/index';
 
 describe('Predefine Static Post', () => {
@@ -30,6 +30,54 @@ describe('Predefine Instance Post', () => {
       expect(created).to.exist;
       expect(created._id).to.eql(predefine._id);
       expect(created.code).to.eql(predefine.code);
+      done(error, created);
+    });
+  });
+
+  after(done => clear(done));
+});
+
+describe('Predefine Relations Static Post', () => {
+  before(done => clear(done));
+
+  const status = Predefine.fake();
+  const predefine = Predefine.fake();
+
+  before(done => create(status, done));
+
+  it('should be able to post', done => {
+    predefine.set({ relations: { status } });
+    Predefine.post(predefine, (error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.eql(predefine._id);
+      expect(created.code).to.eql(predefine.code);
+      expect(created.relations.status).to.exist;
+      expect(created.relations.status).to.eql(status);
+      done(error, created);
+    });
+  });
+
+  after(done => clear(done));
+});
+
+describe('Predefine Relations Instance Post', () => {
+  before(done => clear(done));
+
+  const status = Predefine.fake();
+  const predefine = Predefine.fake();
+
+  before(done => create(status, done));
+
+  it('should be able to post', done => {
+    predefine.set({ relations: { status } });
+    predefine.post((error, created) => {
+      expect(error).to.not.exist;
+      expect(created).to.exist;
+      expect(created._id).to.eql(predefine._id);
+      expect(created.code).to.eql(predefine.code);
+      expect(created.relations.status).to.exist;
+      expect(created.relations.status).to.eql(status);
       done(error, created);
     });
   });
