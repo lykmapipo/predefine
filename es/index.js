3944d49397bf0cdbcd18d379451fddb12b6f009f
lykmapipo <lallyelias87@gmail.com>
2020-04-22 23:38:21 +0300

    chore(deps): force latest version & audit fix

--- es/index.js
+++ es/index.js
@@ -1062,7 +1062,7 @@ const mapToTopoJSON = (...predefines) => {
  * @author lally elias <lallyelias87@gmail.com>
  * @license MIT
  * @since 1.5.0
- * @version 0.1.0
+ * @version 0.2.0
  * @static
  * @public
  * @example
@@ -1092,6 +1092,13 @@ const transformToPredefine = (val) => {
     'namespace',
     'bucket',
     'populate',
+    'strings',
+    'numbers',
+    'booleans',
+    'dates',
+    'geos',
+    'properties',
+    'relations',
   ];
 
   // transform to predefine
@@ -1109,7 +1116,10 @@ const transformToPredefine = (val) => {
     dates: pick(data, ...datePaths),
     geos: pick(data, ...geoPaths),
     relations: pick(data, relationPaths),
-    properties: mergeObjects(data.properties, omit(data, ...knownPaths)),
+    properties: omit(
+      mergeObjects(data.properties, omit(data, ...knownPaths)),
+      ...knownPaths
+    ),
   });
 
   // omit empty paths
